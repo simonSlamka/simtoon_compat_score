43c52c5e52de6bc051f47cf3b9de5498b1d44697
Simon Slamka <simon.slamka@ongakken.com>
2023-12-21 21:43:39 +0100

    instantiate classes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import importlib
 
 attr = importlib.import_module("attraction-classifier.infer").AttractionClassifier()
+userEmbedder = importlib.import_module("simtoon-embeddings.user_embedder").UserEmbedder()
+userUtils = importlib.import_module("simtoon-embeddings.user_utils").UserUtils()
